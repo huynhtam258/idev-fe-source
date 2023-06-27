134ef91c1a67075abc8943e245af6a2c89ffeb1f
Huynh Manh Tam <huynhtam258@gmail.com>
2023-06-27 13:50:48 +0700

    fix: adjust heade and footer layout

--- src/components/templates/FooterComponent.vue
+++ src/components/templates/FooterComponent.vue
@@ -1,9 +1,15 @@
 <template>
-<div class="FooterComponent"></div>
+<div class="footerComponent">
+    <div class="container"></div>
+</div>
 </template>
 
 <script setup lang='ts'>
 </script>
 
 <style lang="scss" scoped>
+.footerComponent {
+    background-color: var(--black1);
+    height: 370px;
+}
 </style>
--- src/components/templates/HeaderComponent.vue
+++ src/components/templates/HeaderComponent.vue
@@ -1,9 +1,43 @@
 <template>
-<div class="HeaderComponent"></div>
+  <div class="headerComponent container">
+    <div class="headerComponent_logo">
+      <img class="icon" src="~/assets/icon/logo.png" alt="" srcset="" />
+      <h4 class="slogan">iDev - luôn là sự tin tưởng của bạn</h4>
+    </div>
+    <div class="headerComponent_nav">
+      <ul class="headerComponent_nav_list">
+        <li>HOME</li>
+        <li>PAGE</li>
+        <li>ABOUT</li>
+        <li>CONTRACT</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script setup lang='ts'>
 </script>
 
 <style lang="scss" scoped>
+.headerComponent {
+  display: flex;
+  justify-content: space-between;
+  &_logo {
+    display: flex;
+    align-items: center;
+    .icon {
+      width: 52px;
+      margin-right: 10px;
+    }
+  }
+  &_nav {
+    &_list {
+      list-style: none;
+      display: flex;
+      li {
+        padding: 10px 20px;
+      }
+    }
+  }
+}
 </style>
