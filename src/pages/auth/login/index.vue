34fc63b569fc4f3c0f38ea5597926860a47fd00c
Huynh Manh Tam <huynhtam258@gmail.com>
2023-07-06 23:27:07 +0700

    feat: Button Component common

--- src/pages/auth/login/index.vue
+++ src/pages/auth/login/index.vue
@@ -6,24 +6,22 @@
           Đăng nhập tài khoản
         </h4>
       </div>
-      <input class="form-control textfield" v-model="email" type="text" placeholder="Tên tài khoản" required>
-      <input class="form-control textfield" v-model="password" type="password" placeholder="Mật khẩu" required>
+      <button-component v-model:modelValue="email" class="textfield" type='text' placeholder='email' />
+      <button-component v-model:modelValue="password" class="textfield" type='password' placeholder='mật khẩu' />
     </form>
     <button class="btn-main-sm" @click="onLogin">Đăng nhập</button>
   </div>
 </template>
 <script lang="ts" setup>
-
 import LoginService from './../../../core/module/login/login.service'
-const email = ref<string>('')
-const password = ref<string>('')
-const loginService = new LoginService()
+
+const email = ref<string>('');
+const password = ref<string>('');
+const loginService = new LoginService();
 
 async function onLogin(): Promise<void> {
   const userLogin = { email: email.value, password: password.value }
   await loginService.login(userLogin)
-  
-  // e.preventDefault()
 }
 </script>
 <style lang="scss" scoped>
@@ -41,33 +39,12 @@ async function onLogin(): Promise<void> {
       margin-bottom: 35px;
       h4 {
         padding-bottom: 20px;
-        border-bottom: 1px solid #cccccc;
+        border-bottom: 1px solid var(--border-color);
         font-weight: 300;
         font-size: 30px;
       }
     }
-    .form-control { 
-      display: block;
-      width: 100%;
-      height: calc(1.5em + 0.75rem + 2px);
-      padding: 0.375rem 0.75rem;
-      font-size: 1rem;
-      font-weight: 400;
-      line-height: 1.5;
-      color: #495057;
-      background-color: #fff;
-      background-clip: padding-box;
-      border: 1px solid #ced4da;
-      border-radius: 0.25rem;
-      transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-      &:focus {
-        border: 1px solid #2e7eed;
-        box-shadow: unset !important;
-      }
-      &:focus-visible {
-        outline: unset;
-      }
-    }
+    
     .textfield {
       margin-bottom: 20px;
     }
@@ -75,8 +52,8 @@ async function onLogin(): Promise<void> {
     .btn-main-sm {
       padding: 15px 35px;
       border-radius: 3px;
-      background: #2e7eed;
-      color: #fff;
+      background: var(--primary-color);
+      color: var(--white);
       outline: none;
       font-size: 14px;
       border: unset;
