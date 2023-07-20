6cf866fb4c1b8080e6951446eb3b9aebc828f6a5
HuynhManhTam <huynhtam258@gmail.com>
2023-07-20 23:01:00 +0700

    feat: slug page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # idev-fe-source
 # node 18.16.0
+WP_URI=https://api.jahid.dev
---
+++ src/pages/blogs/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+</script>
+<template>
+<GradientBannerComponent title="Danh sách bài viết" />
+</template>
+<style lang="scss" scoped>
+</style>
